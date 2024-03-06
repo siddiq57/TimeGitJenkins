177a94863f95ca2511acfd7d9ebd816373fe1967
siddiq57 <98172700+siddiq57@users.noreply.github.com>
2024-03-06 12:31:54 -0500

    Commit First

--- eclipse-workspace/Time/src/time/Time.java
+++ eclipse-workspace/Time/src/time/Time.java
@@ -11,7 +11,7 @@ public class Time {
 	
 	public static void main(String[] args) {
 		int totalSeconds = getTotalSeconds("10:10:10");
-		System.out.println("**GH Total Seconds = " + totalSeconds);
+		System.out.println("**GH Total Second = " + totalSeconds);
 		}
 
 	public static int getTotalSeconds(String time)throws NumberFormatException, StringIndexOutOfBoundsException {
